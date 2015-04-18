fb697b9cb60acb172f8b9ecfaf68610aaadae4a4
andycall <dongtiangche@gmail.com>
2015-04-19 00:53:37 +0800

    place module complete

--- analyse/place.js
+++ analyse/place.js
@@ -1,14 +1,34 @@
 /**
  * Created by andycall on 15/4/16.
  */
-var rblank = /\s\t/g;
-var rfliter =  /(^\w+)|(;<.*)/g;
+var rblank = /[\s\']+/g;
+var rfliter =  /(^[\w=]+)|(;<.*)/g;
 var request = require('request');
+var iconv = require('iconv-lite');
+var util = require('util');
 
 
-//function getPlace(phone, )
+function getPlace(phone, callback) {
+	request({
+		url : 'http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=' + phone,
+		encoding : "base64"
+	}, function(err, response, body){
+		if( ! err && response.statusCode === 200) {
+			var str = iconv.decode(new Buffer(body, 'base64'), 'GBK');
+			var dataObj = str.replace(rblank, '').split('=')[1].split(/[}\{]/)[1].split(',').map(function(value) {
+				var obj = {};
+				obj[value.split(':')[0]] = value.split(':')[1];
+				return obj;
+			});
 
-//request('http://virtual.paipai.com/extinfo/GetMobileProductInfo?mobile=15850781443&amount=10000&callname=getPhoneNumInfoExtCallback', )
+			callback(dataObj);
+		}
+	});
 
+}
+modules.exports = getPlace;
+//getPlace(18523016284, function(dataObj){
+//	console.log(dataObj);
+//});
 
 
